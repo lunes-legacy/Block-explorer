6c803074ffd5ab77c9c8ee778852190061899751
Hellen FS <hellen.ferreira@lunes.io>
2018-05-16 18:48:39 -0300

    IP link changed

--- src/js/config.devnet.js
+++ src/js/config.devnet.js
@@ -2,16 +2,11 @@
     'use strict';
 
     angular.module('web').constant('appConfig', {
-        apiDomain: 'http://34.251.200.245:6869',
+        apiDomain: 'https://testnet.lunes.io/api-docs/index.html',
         title: 'Lunes Devnet Explorer',
         blockchainName: 'Devnet',
         nodes: [
-            {url: 'http://104.27.181.222', maintainer: 'Lunes', showAsLink: true},
-            {url: 'http://34.237.49.199:6869', maintainer: 'Waves', showAsLink: true},
-            {url: 'http://35.157.212.173:6869', maintainer: 'Waves', showAsLink: true},
-            {url: 'http://52.28.115.7:6869', maintainer: 'Waves', showAsLink: true},
-            {url: 'http://13.229.61.140:6869', maintainer: 'Waves', showAsLink: true},
-            {url: 'http://52.192.190.54:6869', maintainer: 'Waves', showAsLink: true}
+            {url: 'https://testnet.lunes.io/api-docs/index.html', maintainer: 'Lunes', showAsLink: true}
         ],
         peerExplorer: {
             url: 'https://testnet.lunes.io/api-docs/index.html',
