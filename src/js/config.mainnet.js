0e64d6769c49cb03eff3dd33af8a540f18b7d2d5
Wandyer Silva <wandyer.s@gmail.com>
2018-06-12 15:14:23 -0300

    Updated URL for testnet node

--- src/js/config.mainnet.js
+++ src/js/config.mainnet.js
@@ -1,28 +1,30 @@
 (function() {
-    'use strict';
+  "use strict";
 
-    angular.module('web').constant('appConfig', {
-        apiDomain: 'https://testnet.lunes.io',
-        blockchainName: 'Mainnet',
-        title: 'Lunes Explorer',
-        nodes: [{
-            url: 'https://testnet.lunes.io',
-            maintainer: 'Lunes'
-        }],
-        peerExplorer: {
-            url: 'https://testnet.lunes.io/api-docs/index.html',
-            title: 'TESTNET Explorer'
-        },
-        wallet: {
-            url: 'https://wallet.lunes.io/',
-            title: 'Wallet'
-        }
-    });
+  angular.module("web").constant("appConfig", {
+    apiDomain: "https://lunesnode-testnet.lunes.io",
+    blockchainName: "Mainnet",
+    title: "Lunes Explorer",
+    nodes: [
+      {
+        url: "https://lunesnode-testnet.lunes.io",
+        maintainer: "Lunes"
+      }
+    ],
+    peerExplorer: {
+      url: "https://lunesnode-testnet.lunes.io/api-docs/index.html",
+      title: "TESTNET Explorer"
+    },
+    wallet: {
+      url: "https://wallet.lunes.io/",
+      title: "Wallet"
+    }
+  });
 
-    angular.module('web').constant('constants.network', {
-        NETWORK_NAME: 'mainnet', // 'devnet', 'testnet', 'mainnet'
-        ADDRESS_VERSION: 1,
-        NETWORK_CODE: 'W',
-        INITIAL_NONCE: 0
-    });
+  angular.module("web").constant("constants.network", {
+    NETWORK_NAME: "testnet", // 'devnet', 'testnet', 'mainnet'
+    ADDRESS_VERSION: 0,
+    NETWORK_CODE: "T",
+    INITIAL_NONCE: 0
+  });
 })();
