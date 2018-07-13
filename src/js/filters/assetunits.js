2430414763de74fbcfc49c50d729fe17fa9afc1c
davidalves1 <alves.david@outlook.com>
2018-07-13 12:05:51 -0300

    Fix the NaN value error when try show the cancel leasing value

--- src/js/filters/assetunits.js
+++ src/js/filters/assetunits.js
@@ -3,7 +3,7 @@
 
     angular.module('web').filter('assetunits', function() {
         return function(input, decimals = 8) {
-            if (input == null) return null;
+            if (input == null || !Number(input)) return null;
             var lunes = Number(input.replace(/\D/, '')) / (10 ** decimals);
             var str = addCommas(lunes.toFixed(decimals));
 
